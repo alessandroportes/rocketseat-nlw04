3f5d768d10ba29bce5d663fcc0f33eda815408b0
Alessandro Portes <alessandro.portes@gmail.com>
2021-02-23 14:13:05 -0300

    Aula 02 - NLW04

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,19 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
+
+import { router } from './routes';
+import './database';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World - NLW04' });
-});
-
-/**
- *  1 parametro => Rota (recurso API)
- *  2 parametro => request e response
- */
-
-app.post('/', (request, response) => {
-  //Recebeu os dados para salvar
-  return response.json({ message: 'Os dados foram salvos com sucesso!' });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running!'));
